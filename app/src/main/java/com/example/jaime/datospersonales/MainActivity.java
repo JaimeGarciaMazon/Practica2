29584a56779626f1639ea4877882c32526dc845b
jaime <jaimegarcia_24@hotmail.com>
2018-11-07 19:55:28 +0100

    Subida de Informacion

--- app/src/main/java/com/example/jaime/datospersonales/MainActivity.java
+++ app/src/main/java/com/example/jaime/datospersonales/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.jaime.datospersonales;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
 
 
@@ -24,6 +29,13 @@ public class MainActivity extends AppCompatActivity {
     RadioButton buttonMujer;
     TextView txtEstadoCivil;
     Spinner SpinnerEstadoCivil;
+    TextView txtHijos;
+    Button btnGenerar;
+    ImageButton btnEliminar;
+    TextView txtInformacion;
+    Switch idswitch;
+    String Genero = "";
+    String switch1 = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +48,72 @@ public class MainActivity extends AppCompatActivity {
         grupoGenero = findViewById(R.id.grupoGenero);
         buttonHombre = findViewById(R.id.buttonHombre);
         buttonMujer = findViewById(R.id.buttonMujer);
-        txtEstadoCivil = findViewById(R.id.txtEstadoCivil);
         SpinnerEstadoCivil = findViewById(R.id.spinnerEstadoCivil);
+        btnGenerar = findViewById(R.id.btnGenerar);
+        btnEliminar = findViewById(R.id.btnEliminar);
+        idswitch = findViewById(R.id.idswitch);
+        txtInformacion = findViewById(R.id.txtInformacion);
+
+
+        String[] informacion = new String[] {"","Casado", "Separado", "Viudo", "Otro"};
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, informacion);
+        SpinnerEstadoCivil.setAdapter(adaptador);
+
+
+        btnEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+
+
+                editNombre.setText("");
+                editApellidos.setText("");
+                editEdad.setText("");
+                buttonHombre.setChecked(false);
+                buttonMujer.setChecked(false);
+                SpinnerEstadoCivil.setSelection(0);
+                idswitch.setChecked(false);
+            }
+
+        });
+
+        grupoGenero.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.buttonHombre) Genero = "Hombre";
+                if (checkedId == R.id.buttonMujer)  Genero = "Mujer";
+            }
+        });
+
+        idswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (idswitch.isChecked() == true)
+                {
+                    switch1 = "Si tiene Hijos" ;
+                }
+                if (idswitch.isChecked() == false)
+                {
+                    switch1 = "No tiene Hijos" ;
+                }
+
+            }
+        });
+
+        btnGenerar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+
+
+                txtInformacion.setText(editApellidos.getText().toString() + " " + editNombre.getText().toString() + " " + editEdad.getText().toString() + " " + Genero + " " + switch1 + " " );
+
+
+            }
+        });
+
     }
-  int a = 0;
 }
+
